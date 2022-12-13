f5dd4658daeaf42d67faefd3e1ecc5772545017c
Rajitkumar <krajit909@gmail.com>
2022-12-13 21:33:28 +0530

    Loading spinner added

--- frontend/src/Components/LoadingLogo.js
+++ frontend/src/Components/LoadingLogo.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import HashLoader from 'react-spinners/HashLoader'
 
-function LoadingLogo() {
+function LoadingLogo({loading}) {
+    const override = {
+        display: "Block",
+        margin: "40px auto"
+    };
   return (
-    <div>LoadingLogo</div>
-  )
+    <div>
+        <HashLoader
+        color={'#35bdd0'}
+        loading={loading}
+        cssOverride={override}
+        size={50}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        />
+        <h1 className='text-lg mx-auto text-center font-semibold'>Loading...</h1>
+    </div>
+    )
 }
 
 export default LoadingLogo
