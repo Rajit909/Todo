5f0006fa0209175068f725b49dbf52345ba7d1e2
Rajitkumar <krajit909@gmail.com>
2022-12-14 22:45:37 +0530

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
-
 import AddTodo from './Components/AddTodo';
 import TodoHero from './Components/TodoHero';
 import Navbar from './Components/Navbar';
